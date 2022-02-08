efb282a8f6ce7c013ba1b1ee1246e975e021c5f6
voskaul <voskaul5058@adcrow.tech>
2022-02-08 14:31:43 +0800

    reset

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import "./style.css"
-import PinterestLayout from "./pinterest/PinterestLayout"
-import PinterestLayout2 from "./GsapTest/GsapTest"
-import Test from "./Test/Test"
-import "./scss/style.scss"
-
-function Header() {
-  return <PinterestLayout />
-}
-
-function Menu() {
-  return <div className="menu">list1</div>
-}
-
-function Layout(props) {
-  return (
-    <div>
-      <div className="Layout">
-        <div className="layout-header">{props.header}</div>
-        <div className="layout-menu">{props.menu}</div>
-      </div>
-    </div>
-  )
-}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Test />
-
-      <span>------</span>
-      <PinterestLayout2 />
-      <span>------</span>
-      <Layout header={<Header />} menu={<Menu />} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
